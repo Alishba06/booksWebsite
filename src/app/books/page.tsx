87ec6c1168635cc063917b37b509b6cc9f77d028
Alishba06 <alishbainteligent46@gmail.com>
2024-10-23 22:30:09 +0500

    first commit

---
+++ src/app/books/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Book() {
+  return (
+    <main>
+        
+    </main>
+  )
+}
+
+export default Book
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+   <main>
+    <h1>Tehreem</h1>
+   </main>
+  );
+}
